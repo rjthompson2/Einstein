e6a95a25bafa205d0af6022d39af1c10fd31783b
Riley Thompson <rjthompson2@dons.usfca.edu>
2022-07-26 20:13:19 -0700

    minor changes

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ time.sleep(2)
 listener = Listener()
 commands = Commands(listener)
 commands.respond("Hi, what can I do for you?")
-listening = True
 while listener.listening:
     data = listener.listen()
     commands.understand(data)
