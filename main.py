8353d3d1bdf2b3f412549fed16afc7b2cc7f2fea
Riley Thompson <rjthompson2@dons.usfca.edu>
2022-08-03 19:06:53 -0700

    moved respond to utils

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import time
 from commands import Commands
 from listener import Listener
+from utils import respond
 
 time.sleep(2)
 listener = Listener()
 commands = Commands(listener)
-commands.respond("Hi, what can I do for you?")
+respond("Hi, what can I do for you?")
 while listener.listening:
     data = listener.listen()
     commands.understand(data)
--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 from gtts import gTTS
+import os
 
-def respond(self, audioString:str) -> None:
+def respond(audioString:str) -> None:
     print(audioString)
     tts = gTTS(text=audioString, lang='en')
     tts.save("speech.mp3")
